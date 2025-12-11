908fc1c2a23bbfffe8173d524436e19aabb8af55
Antonio Ceppellini <antonio.ceppellini@gmail.com>
2025-12-11 18:19:50 +0100

    more of functions

--- functions.go
+++ functions.go
@@ -9,3 +9,37 @@ func sub(x int, y int) int {  // this is the function signature:
 func sub(x, y int) int {
   return x-y
 }
+
+// if a function has multiple return values
+// we need to wrap them like that:
+
+func fullName() (string, string){
+  return "Spider", "Man"
+}
+
+// variables aren't changed in a function
+// a function works with a copy of a variable:
+
+x := 15
+
+func increment(x int) int {
+  return x+1
+}
+
+increment(x)
+
+fmt.Println(x) // -> output will be 15
+// but if we do:
+x = increment(x)
+
+fmt.Println(x) // -> output will be 1
+
+// IMPORTANT:
+// go doesn't allow us to have unused variables
+// we can ignore a variable doing like that:
+
+func getPoint() (int, int){
+  return 2, 3
+}
+
+x, _ = getPoint()
